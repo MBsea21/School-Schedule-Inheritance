83dbe2047aabd0c85d50b25cd08f032fea9c4886
Madeline Bennett <madeline.bennett.pm@gmail.com>
2024-10-03 04:27:16 +1000

    finished middle school student

--- school_schedule/middle_school_student.py
+++ school_schedule/middle_school_student.py
@@ -1,3 +1,23 @@
 from .student import Student
 
+class MiddleSchoolStudent(Student):
+    def __init__(self,name,grade,classes,gets_transportation=False):
+        super().__init__(name,grade,classes)
+        self.gets_transportation=gets_transportation
+
+    def gets_transportation_message(self):
+        if self.gets_transportation is True:
+            return f"{self.name} gets ride to school"
+        else: 
+            return f"{self.name} does not get ride to school"
+
+    def summary(self):
+        student_summary = super().summary()
+        transportation_message=self.gets_transportation_message()
+        return "\n".join((student_summary,transportation_message))
+
+
+
+        
+
 # add MiddleSchoolStudent here
